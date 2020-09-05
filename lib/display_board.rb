1ebdf65c4fd0972cf4df0938c76def148921faf9
John Troutman <jtroutmanman@gmail.com>
2020-09-05 18:28:21 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,12 @@
 # Define display_board that accepts a board and prints
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+tictactoe = [" ", "X", "O", " ", "X", "O", " ", " ", " "]
 # out the current state.
+display_board(tictactoe)
